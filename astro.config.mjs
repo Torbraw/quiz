c530846e3f5efecfc4a3c1e3a2a52ec8c43e4daa
Maxime <gendron.maxime1@gmail.com>
2024-12-01 14:55:10 -0500

    ci: test fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import solidJs from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false }), solidJs()],
-  base: import.meta.env.MODE === "development" ? "" : "quiz",
+  base: "quiz",
   i18n: {
     defaultLocale: "fr",
     locales: ["fr", "en"],
